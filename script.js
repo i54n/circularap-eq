f780d34cd838818ec28f8ac8b7fcec9098ccc5f6
i54n <56086253+i54n@users.noreply.github.com>
2020-09-29 22:46:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,38 @@
-$('.js-audio').each(function (index, el) {
-  initAudioPlayer($(this), index);
-});
-
-$('.audio__slider').roundSlider({
-  radius: 50,
-  value: 0,
-  startAngle: 90,
-  width: 5,
-  handleSize: '+6',
-  handleShape: 'round',
-  sliderType: 'min-range' });
-
-
-$('.audio__slider').on('drag, change', function (e) {
-  let $this = $(this);
-  let $elem = $this.closest('.js-audio');
-  updateAudio(e, $elem);
-  $this.addClass('active');
-});
-
-function updateAudio(e, $elem) {
-  console.log(e.handle.value);
-  let value = e.handle.value;
-  // var thisPlayer = el.find('.js-audio'),
-  var play = $elem.find('.play-pause'),
-  circle = $elem.find('#seekbar'),
-
-  getCircle = circle.get(0),
-  totalLength = getCircle.getTotalLength(),
-  //currentTime = $elem.find('audio')[0].currentTime,
-  maxduration = $elem.find('audio')[0].duration;
-  var y = value * maxduration / 100;
-  $elem.find('audio')[0].currentTime = y;
-
+function pad(number) {
+	if(number < 10) {
+		return "0" + number
+	} else {
+		return number;
+	}
 }
 
-function initAudioPlayer(player) {
-  let audio = player.find('audio'),
-  play = player.find('.play-pause'),
-  circle = player.find('#seekbar'),
-  getCircle = circle.get(0),
-  totalLength = getCircle.getTotalLength();
-
-
-  circle.attr({
-    'stroke-dasharray': totalLength,
-    'stroke-dashoffset': totalLength });
-
-
-  play.on('click', () => {
-    if (audio[0].paused) {
-      $('audio').each((index, el) => {
-        $('audio')[index].pause();
-      });
-      $('.js-audio').removeClass('playing');
-      audio[0].play();
-      player.removeClass('paused');
-      player.addClass('playing');
-    } else {
-      audio[0].pause();
-      player.removeClass('playing');
-      player.addClass('paused');
-    }
-  });
-
-  audio.on('timeupdate', () => {
-    let currentTime = audio[0].currentTime,
-    maxduration = audio[0].duration,
-    calc = totalLength - currentTime / maxduration * totalLength;
-
-    circle.attr('stroke-dashoffset', calc);
-
-    let value = Math.floor(currentTime / maxduration * 100);
-
-    var slider = audio.closest('.js-audio').find('.audio__slider');
-    $(slider).roundSlider('setValue', value);
-  });
-
-  audio.on('ended', () => {
-    player.removeClass('playing');
-    circle.attr('stroke-dashoffset', totalLength);
-
-  });
+function resetPlayer() {
+	console.log($("#pie").css("animation-delay"));
 }
+
+$(function () {
+	$("#player").on("timeupdate", function () {
+		var percent = Math.round((this.currentTime / this.duration * 100), 2);
+		$("#pie").css({
+			"animation-delay": "-" + percent + "s"
+		});
+		
+		var seconds = Math.round(this.currentTime % 60, 0);
+		var minutes = parseInt(this.currentTime / 60, 10);
+		$(".time").html(pad(minutes) + ":" + pad(seconds));
+	});
+	
+	$(".play").on("click", function () {
+		$("#player")[0].play();
+		$(this).hide();
+		$(".pause").show();
+	});
+	
+	$(".pause").on("click", function () {
+		$("#player")[0].pause();
+		$(this).hide();
+		$(".play").show();
+	});
+	
+});
+
